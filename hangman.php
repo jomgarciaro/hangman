71f95429d348e29988555cc0467c5ca0617c2315
jomgarciaro <jomgarciaro@unal.edu.co>
2021-11-26 11:40:42 -0500

    feat: agregar feedback al usuario

--- hangman.php
+++ hangman.php
@@ -8,4 +8,49 @@ if ($argc !== 2) {
 $secretWord = $argv[1];
 
 echo 'Bienvenido al juego del ahorcado.' . PHP_EOL;
-echo 'La palabra secreta tiene ' . mb_strlen($secretWord) . ' letras.' . PHP_EOL;
+echo 'La palabra tiene ' . mb_strlen($secretWord) . ' letras' . PHP_EOL;
+
+const MAX_ATTEMPS = 10;
+
+$attemps = 0;
+
+$lettersGuessed = [];
+
+do {
+
+    echo 'Por favor digite una letra. ' . PHP_EOL;
+    $letter = trim(fgets(STDIN));
+
+    $letters = str_split_unicode($secretWord);
+
+    if (in_array($letter, $letters)) {
+        $lettersGuessed[] = $letter;
+        echo 'Correcto: ' . get_guessed_word($secretWord, $lettersGuessed) . PHP_EOL;
+    } else {
+        echo 'Incorrecto: ' .  get_guessed_word($secretWord, $lettersGuessed) . PHP_EOL;
+    }
+
+    $attemps++;
+
+} while($attemps < MAX_ATTEMPS);
+
+
+function str_split_unicode($str) {
+    return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+}
+
+function get_guessed_word($secretWord, $lettersGuessed) {
+    
+    $letters = str_split_unicode($secretWord);
+    $guessedWord = '';
+
+    foreach ($letters as $letter) {
+        if (in_array($letter, $lettersGuessed)) {
+            $guessedWord .= $letter;
+        } else {
+            $guessedWord .= '_';
+        }
+    }
+
+    return $guessedWord; 
+}
